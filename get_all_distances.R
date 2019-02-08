4c31cf464ad1d878309db990b22331e1f0cba9b7
evanythn <evan.ythn@gmail.com>
2019-02-08 11:36:10 -0800

    finished first go of docstring

--- get_all_distances.R
+++ get_all_distances.R
@@ -1,8 +1,8 @@
-#' Return distance
+#' Return distance/similarity metric for each row in a dataframe
 #'
-#' Creates a new factor from two existing factors, where the new factors levels are the union of levels of the input factors
+#' Compares an input reference vector to all rows of an input data frame, calculating the specified distance/similarity metric for each row.
 #'
-#' @param a data frame of size n by k
+#' @param df_comparison data frame of size n by k
 #' @param ref_vec vector of length k
 #' @param dist_type string
 #'
@@ -12,8 +12,8 @@
 #' @examples
 #'
 
-get_all_distances <- function(, ref_vec, ){
+get_all_distances <- function(df_comparison, ref_vec, dist_type = "euclidean"){
 
 
-
+      return(as.numeric(c()))
 }

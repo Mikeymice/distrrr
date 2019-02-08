113bc24cc4ee374abf5856a75e7cb4e4f95a45a2
evanythn <evan.ythn@gmail.com>
2019-02-08 11:26:24 -0800

    created get_all_distances script, partially filled out documentation

---
+++ get_all_distances.R
@@ -0,0 +1,19 @@
+#' Return distance
+#'
+#' Creates a new factor from two existing factors, where the new factors levels are the union of levels of the input factors
+#'
+#' @param a data frame of size n by k
+#' @param ref_vec vector of length k
+#' @param dist_type string
+#'
+#' @return nmueric vector of length n
+#' @export
+#'
+#' @examples
+#'
+
+get_all_distances <- function(, ref_vec, ){
+
+
+
+}

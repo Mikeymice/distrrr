cb3994bb8a59e81ce35175c2912648f8f04eee02
evanythn <evan.ythn@gmail.com>
2019-02-08 14:20:52 -0800

    changed naming of parameters to be consistent with other functions

--- get_all_distances.R
+++ get_all_distances.R
@@ -4,7 +4,7 @@
 #'
 #' Compares an input reference vector to all rows of an input data frame, calculating the specified distance/similarity metric for each row.
 #'
-#' @param df_comparison data frame of size n by k
+#' @param data data frame of size n by k
 #' @param ref_vec vector of length k
 #' @param dist_type string
 #'
@@ -14,7 +14,7 @@
 #' @examples
 #'
 
-get_all_distances <- function(df_comparison, ref_vec, dist_type = "euclidean"){
+get_all_distances <- function(data, ref_vec, dist_type = "euclidean"){
 
       return(as.numeric(c()))
 }

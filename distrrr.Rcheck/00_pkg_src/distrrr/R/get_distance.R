8adc752da24d7314607fb0ce6dd8c92bdd587f07
Mike Yuan <yuan.mike84@gmail.com>
2019-02-08 22:14:33 -0800

    added basic test for test_get_distance.R

--- distrrr.Rcheck/00_pkg_src/distrrr/R/get_distance.R
+++ distrrr.Rcheck/00_pkg_src/distrrr/R/get_distance.R
@@ -1,15 +1,18 @@
 
 #' get_distance
 #'
-#' @param point1
-#' @param point2
-#' @param metric
+#' @param point1 vector with numeric values
+#' @param point2 vector with numeric values
+#' @param metric defuault "euclidean", "cosine" will return cosine similiarity distnace, and "manhhantan" will return manhhantan distance
 #'
 #' @return float, the distance between point1 and point2 based on the metric given
 #' @export
 #'
 #' @examples
-get_distance <- function(point1, point2, metric)
+#'
+#' get_distance(c(0,0,0), c(1,0,0)) should return 1
+#'
+get_distance <- function(point1, point2, metric = "euclidean")
 {
 
   return(-1) # dummy result
---
+++ distrrr.Rcheck/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(distrrr)
+
+test_check("distrrr")

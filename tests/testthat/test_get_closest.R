527d05a4e17cfe61f07bbb3de78a26d561dac21c
evanythn <evan.ythn@gmail.com>
2019-02-26 17:13:37 -0800

    fixed syntax for test_that functions

--- tests/testthat/test_get_closest.R
+++ tests/testthat/test_get_closest.R
@@ -17,29 +17,29 @@ test_that("Output should be a vector containing numbers", {
 })
 
 
-test_that("Output should have a length of k"){
+test_that("Output should have a length of k",{
   if (length(df) >= k){
     expect_equal(length(get_closest(x, df, k)), k)
   }
   else {
     expect_equal(length(get_closest(x, df, k)), length(df))
   }
-}
+})
 
-test_that("All numbers in output vector should be non-negative"){
+test_that("All numbers in output vector should be non-negative",{
   output <- get_closest(x, df, k)
   expect_true(all(output>=0))
-}
+})
 
-test_that("Input data parameter should be a dataframe"){
+test_that("Input data parameter should be a dataframe",{
   expect_error(get_closest(x, 8, k))
-}
+})
 
-test_that("Input parameter top_k should be a non-negative integer"){
+test_that("Input parameter top_k should be a non-negative integer",{
   expect_error(get_closest(x, df, -2))
-}
+})
 
-test_that("Input parameter point should be a vector"){
+test_that("Input parameter point should be a vector",{
   expect_error(get_closest(7, df, k))
-}
+})
 

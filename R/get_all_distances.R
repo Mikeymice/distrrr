ad6d4333951138d521d24ffa05b3d48b020f0428
evanythn <evan.ythn@gmail.com>
2019-03-01 20:27:08 -0800

    added example and fixed function description to be consistent

--- R/get_all_distances.R
+++ R/get_all_distances.R
@@ -4,15 +4,16 @@
 #'
 #' Compares an input reference vector to all rows of an input data frame, calculating the specified distance/similarity metric for each row.
 #'
-#' @param data data frame of size n by k to compare ref_vec to
 #' @param point vector of length k to compare to the data frame
-#' @param dist string indicating type of distance metric
+#' @param data data frame of size n by k to compare ref_vec to
+#' @param metric string, default value "euclidean" for euclidean distance, "cosine" will return cosine similiarity distance, and "manhattan" will return manhattan distance
 #'
 #' @return numeric vector of length n
 #' @export
 #'
 #' @examples
-#'
+#' df <- data.frame("A" = c(1,2,3), "B" = c(8,2,4))
+#' ref_vec <- c(-2,4)
 #'
 
 get_all_distances <- function(point, data, metric = "euclidean"){
